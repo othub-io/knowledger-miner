ceb14fcf97d61c461ccba1d494f6773adb21ece1
CosmiCloud <tybranha@gmail.com>
2024-06-23 13:52:18 -0400

    first commit

--- util/queryDB.js
+++ util/queryDB.js
@@ -1,8 +1,8 @@
-const paranet_db = require("../database/paranet_db");
+const paranet_miner_db = require("../database/paranet_miner_db");
 
 module.exports = executeQuery = async (query, params) => {
   return new Promise(async (resolve, reject) => {
-    await paranet_db.query(query, params, (error, results) => {
+    await paranet_miner_db.query(query, params, (error, results) => {
       if (error) {
         reject(error);
       } else {
