74adfa2da68077bcae476f9726fb57efbde6dccb
CosmiCloud <tybranha@gmail.com>
2024-06-25 16:49:43 -0400

    updated service to log to journal

--- knowledger-processor.sh
+++ knowledger-processor.sh
@@ -3,9 +3,9 @@
 # Change to the directory containing your Node.js application
 cd ~/knowledger-miner/src/processor
 
-# Start the application using nodemon and log output
+# Start the application using nodemon and log output to stdout and stderr
 while true
 do
-    nodemon start >> /var/log/knowledger-processor.log 2>&1
+    nodemon start
     sleep 1
 done
